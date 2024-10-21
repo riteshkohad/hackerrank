e4c6ba5afe0040f5c40a9edf2897263e8fae9c8f
Ritesh Kohad <ritesh.kohad@adp.com>
2024-10-21 16:25:17 -0700

    file updated

--- LeetCode/Top 150/Jump Game II.py
+++ LeetCode/Top 150/Jump Game II.py
@@ -5,12 +5,12 @@ def jump(nums) -> int:
     total_jumps = 0
 
     while r < len(nums) - 1:
-        max_jump = 0
+        long_jump = 0
         for i in range(l, r +1):
-            max_jump = max(max_jump, i + nums[i])
+            long_jump = max(long_jump, i + nums[i])
 
         l = r + 1
-        r = max_jump
+        r = long_jump
         total_jumps += 1
     
     return total_jumps
